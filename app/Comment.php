3b70a6eeb39303ce88dbf13e148e49aa55f01e68
chenyirou <annie851216@gmail.com>
2017-11-13 19:35:34 +0800

    開啟 Comment.php，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App;
-
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+public function post()
+{
+return $this->belongsTo(Post::class);
+}
 }

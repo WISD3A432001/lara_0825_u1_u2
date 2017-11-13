678170dee73223add1762221cc8fdf3f7179893a
chenyirou <annie851216@gmail.com>
2017-11-13 19:03:48 +0800

    使用 update方法

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,15 @@ Route::get('test', function () {
     dd($post);
 */
 
+/*
     $posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
     dd($posts);
+*/
+
+    $post=\App\Post::find(1);
+    $post->update([
+        'title'=>'updated title',
+        'content'=>'updated content',
+    ]);
 
 });

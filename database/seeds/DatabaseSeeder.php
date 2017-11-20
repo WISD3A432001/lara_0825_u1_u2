ef26b2cc328179e4c3971587188e8c7f6a466673
chenyirou <annie851216@gmail.com>
2017-11-20 16:17:24 +0800

    將 seeder 設定至 DatabaseSeeder內

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CommentsTableSeeder::class);
+        //$this->call(PostsTableSeeder::class);   之前做過
         $this->call(PostsTableSeeder::class);
     }
 }

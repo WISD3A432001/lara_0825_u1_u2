df44b7de23f67e8f28e40064788966476387922a
chenyirou <annie851216@gmail.com>
2017-11-20 16:39:10 +0800

    使用 Faker 讓資料更擬真

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 use App\Post;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 class PostsTableSeeder extends Seeder
 {
@@ -12,14 +13,19 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         Post::truncate();
+        $faker=Faker::create('zh_TW');
         foreach(range(1,20) as $number){
             $total=20;
             Post::create([
-            'title' => 'title' .$number,
+            /*
+                'title' => 'title' .$number,
                 'content' => 'content' .$number,
                 'is_feature' => rand(0,1),
                 'created_at' => Carbon::now() ->subDays($total - $number),
                 'updated_at' => Carbon::now() ->subDays($total - $number),
+            */
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph,
             ]);
         }
     }
